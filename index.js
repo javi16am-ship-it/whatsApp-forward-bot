fc094d4de1f3d35d21f79fba79deed978b338fd6
javi16am-ship-it <javi16am@gmail.com>
2025-11-18 18:48:54 -0500

    require catch

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
-const qrcode = require('qrcode-terminal');
-const pino = require('pino');
+try {
+  console.log('>>> CARGANDO MODULOS...');
+  const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+  const qrcode = require('qrcode-terminal');
+  const pino = require('pino');
+  console.log('>>> MODULOS OK');
 
-async function start() {
-  console.log('>>> INICIANDO BOT...');
-  const { state, saveCreds } = await useMultiFileAuthState('auth_info');
-  console.log('>>> Auth cargado');
-  const sock = makeWASocket({
-    auth: state,
-    logger: pino({ level: 'silent' })
+  async function start() {
+    console.log('>>> INICIANDO BOT...');
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info');
+    console.log('>>> Auth cargado');
+    const sock = makeWASocket({
+      auth: state,
+      logger: pino({ level: 'silent' })
+    });
+    console.log('>>> Socket creado');
+
+    sock.ev.on('creds.update', saveCreds);
+    sock.ev.on('connection.update', ({ qr, connection }) => {
+      if (qr) {
+        console.log('>>> QR GENERADO:');
+        qrcode.generate(qr, { small: true });
+      }
+      if (connection === 'open') console.log('>>> BOT CONECTADO');
+    });
+  }
+
+  start().catch(err => {
+    console.error('>>> ERROR CAPTURADO:', err.message || err);
+    console.error(err.stack);
+    process.exit(1);
   });
-  console.log('>>> Socket creado');
-
-  sock.ev.on('creds.update', saveCreds);
-  sock.ev.on('connection.update', ({ qr, connection }) => {
-    if (qr) {
-      console.log('>>> QR GENERADO:');
-      qrcode.generate(qr, { small: true });
-    }
-    if (connection === 'open') console.log('>>> BOT CONECTADO');
-  });
-}
-
-start().catch(err => {
-  console.error('>>> ERROR CAPTURADO:', err.message || err);
-  console.error(err.stack);
+} catch (reqErr) {
+  console.error('>>> FALLO EN REQUIRE:', reqErr.message || reqErr);
   process.exit(1);
-});
+}
