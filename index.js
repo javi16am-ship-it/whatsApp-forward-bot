02055db98905f7fa7c4c8b223047de1cb48c6d10
javi16am-ship-it <javi16am@gmail.com>
2025-11-18 18:38:43 -0500

    catch full

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const pino = require('pino');
 async function start() {
   console.log('>>> INICIANDO BOT...');
   const { state, saveCreds } = await useMultiFileAuthState('auth_info');
+  console.log('>>> Auth cargado');
   const sock = makeWASocket({
     auth: state,
     logger: pino({ level: 'silent' })
   });
+  console.log('>>> Socket creado');
 
   sock.ev.on('creds.update', saveCreds);
   sock.ev.on('connection.update', ({ qr, connection }) => {
@@ -21,7 +23,7 @@ async function start() {
 }
 
 start().catch(err => {
-  console.error('>>> ERROR FATAL:', err.message);
+  console.error('>>> ERROR CAPTURADO:', err.message || err);
   console.error(err.stack);
   process.exit(1);
 });
