2418ce39c06ccf9bdf9c5c72e10ce776f05f5737
javi16am-ship-it <javi16am@gmail.com>
2025-11-18 19:00:21 -0500

    no persist

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-try {
-  console.log('>>> CARGANDO MODULOS...');
-  const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
-  const qrcode = require('qrcode-terminal');
-  const pino = require('pino');
-  console.log('>>> MODULOS OK');
+const { default: makeWASocket } = require('@whiskeysockets/baileys');
+const qrcode = require('qrcode-terminal');
+const pino = require('pino');
 
-  async function start() {
-    console.log('>>> INICIANDO BOT...');
-    const { state, saveCreds } = await useMultiFileAuthState('./tmp_auth');
-    console.log('>>> Auth cargado');
-    const sock = makeWASocket({
-      auth: state,
-      logger: pino({ level: 'silent' })
-    });
-    console.log('>>> Socket creado');
+console.log('>>> INICIANDO BOT...');
+const sock = makeWASocket({
+  logger: pino({ level: 'silent' })
+});
 
-    sock.ev.on('creds.update', saveCreds);
-    sock.ev.on('connection.update', ({ qr, connection }) => {
-      if (qr) {
-        console.log('>>> QR GENERADO:');
-        qrcode.generate(qr, { small: true });
-      }
-      if (connection === 'open') console.log('>>> BOT CONECTADO');
-    });
+sock.ev.on('connection.update', ({ qr, connection }) => {
+  if (qr) {
+    console.log('>>> QR GENERADO:');
+    qrcode.generate(qr, { small: true });
   }
-
-  start().catch(err => {
-    console.error('>>> ERROR CAPTURADO:', err.message || err);
-    console.error(err.stack);
-    process.exit(1);
-  });
-} catch (reqErr) {
-  console.error('>>> FALLO EN REQUIRE:', reqErr.message || reqErr);
-  process.exit(1);
-}
+  if (connection === 'open') console.log('>>> BOT CONECTADO');
+});
